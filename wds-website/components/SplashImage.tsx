38d27f387fb9654ae3585334f071fcf8f482818e
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-10-11 02:31:04 +0100

    updating animation timing

--- wds-website/components/SplashImage.tsx
+++ wds-website/components/SplashImage.tsx
@@ -12,10 +12,10 @@ export const SplashImage = () => {
         }
        if(hasPageMounted){
         animate(imageRef.current!,{
-            y:[30,-25,-120],
+            y:[30,-45,-120],
             opacity:[0,0,1],
             scale:[0.8,1]
-        },{duration:0.4,ease:"easeOut",times:[0,0.5,1]});
+        },{duration:0.7,ease:"easeOut",times:[0,0.5,1]});
         setTimeout(() => {
         animate(imageRef.current!,{
             scale:[1,1.05,0.95]
