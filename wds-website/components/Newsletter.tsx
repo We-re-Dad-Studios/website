263de164d2bb1f242fda2ae8fe6c8d6d7f57f721
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-10-12 18:38:13 +0100

    try to fix dropdown

--- wds-website/components/Newsletter.tsx
+++ wds-website/components/Newsletter.tsx
@@ -6,7 +6,7 @@ export const Newsletter = () => {
   return (
     <div className='w-full xl:w-[80vw] mx-auto rounded-lg flex mb-12 overflow-hidden px-2'>
      <div className="flex-1  rounded-l-lg flex relative bg-black">
-     <div className='grid place-items-center px-4 border-r-[2px] bg-primary-0  border-r-white news-1  absolute left-0 top-0 z-[10] h-full'>
+     <div className=' place-items-center px-4 border-r-[2px] bg-primary-0  border-r-white news-1  absolute left-0 top-0 z-[10] h-full lg:grid hidden'>
      <Image className='lg:w-40 md:w-32 w-24 aspect-square lg:h-40 my-auto' alt='WDS Logo' src={"/images/WDS LOGO WHITE.png"} width={1000} height={1000}/>
      </div>
     <div className='relative flex-1 lg:block hidden overflow-hidden'>
@@ -16,7 +16,7 @@ export const Newsletter = () => {
      <Image alt='John Doe' className='z-[0]  absolute right-0 top-0 w-1/3 h-full object-cover object-left' width={1000} height={1000} src={"/images/jd-2.jpg"}/>
     </div>
      </div>
-     <div className='ml-auto min-h-[50vh] py-4 h-max bg-primary-100 w-max lg:px-0 max-w-[70%] lg:w-[35%] flex flex-col justify-center items-center rounded-r-lg'>
+     <div className='ml-auto min-h-[50vh] py-4 h-max bg-primary-100  lg:px-0 md:max-w-[70%] w-full lg:w-[35%] flex flex-col justify-center items-center rounded-r-lg'>
         <p className='bg-[#F21170] px-2 xl:px-4 py-2 rounded-lg border-[2px] border-black text-[18px] whitespace-nowrap lg:text-[28px]'>JOIN THE ADVENTURE - SUBSCRIBE NOW!</p>
         <p className='w-[90%] lg:w-[70%] text-center text-neutral_900 lg:text-[18px] text-[14px] mt-4'>Be the first to know about our latest releases, exclusive content, and behind-the-scenes updates. just for you. Subscribe today and never miss out!</p>
      <SendEmail/>
