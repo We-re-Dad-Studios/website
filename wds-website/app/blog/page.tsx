446d09f4ca6fc24f5717e31788ddcfbb516e9c87
ehiosu <53658556+ehiosu@users.noreply.github.com>
2025-03-10 15:55:54 +0100

    change url on metadata export

--- wds-website/app/blog/page.tsx
+++ wds-website/app/blog/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata() {
         siteName: "WDS",
         images: [
           {
-            url: "https://weredadstudios.netlify.app/images/android-chrome-512x512.png",
+            url: "https://weredadstudios.netlify.app/images/WDS%20LOGO%20BLACK_.png",
             width: 1200,
             height: 630,
             alt: "WDS Blog",
