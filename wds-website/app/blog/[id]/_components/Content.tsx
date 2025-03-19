40688ba3d8d28f55c1a5e2348975cc694b9d5626
ehiosu <53658556+ehiosu@users.noreply.github.com>
2025-03-19 12:40:27 +0100

    style: improve layout and styling of Content component

--- wds-website/app/blog/[id]/_components/Content.tsx
+++ wds-website/app/blog/[id]/_components/Content.tsx
@@ -4,11 +4,11 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { Document } from "@contentful/rich-text-types";
 const Content = ({content,title}:{content:string,title:string}) => {
   return (
-    <div className="my-8 w-full leading-[30px] lg:px-0 px-2  lg:w-[80%] mx-auto">
-      <p className="text-3xl  mb-4 mt-2 font-bold tracking-wider text-center">
+    <div className="my-8 w-full leading-[50px] lg:px-0  text-[20px] text-neutral-400   lg:w-[80%] mx-auto bg-white/10 backdrop-blur-sm p-2 px-4">
+     <div className="w-[90%] mx-auto"> <p className="text-3xl  mb-4 mt-2 font-bold tracking-wider text-center">
         {title}
       </p>
-        {documentToReactComponents(content as unknown as Document)}
+        {documentToReactComponents(content as unknown as Document)}</div>
 
     </div>
   )
