cd6b1a12f0bf8c15422472ac897b7f020d9d55c3
ehiosu <53658556+ehiosu@users.noreply.github.com>
2025-05-05 09:38:04 +0100

    feat: add next-sitemap configuration and dependencies for sitemap generation

---
+++ wds-website/next-sitemap.config.js
@@ -0,0 +1,11 @@
+
+const config = {
+    siteUrl: 'https://www.weredadstudios.com', // replace with your domain
+    generateRobotsTxt: true, // optional but recommended
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 7000,
+  };
+
+  export default config;
+  
